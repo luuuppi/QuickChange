fb9a9a81f9b9b8ec02afde5d3173132ed4876fa5
Mikhail <Mikhail.Yakushinale@Gmail.com>
2024-06-29 16:44:20 +0700

    feat: implemented logic for switching currencies

--- src/modules/ConvertForm/useConvertSrote.ts
+++ src/modules/ConvertForm/useConvertSrote.ts
@@ -1,22 +1,29 @@
 import { create } from "zustand";
 import { TCurrencyInfo } from "./types/TCurrencyInfo";
+import { devtools } from "zustand/middleware";
 
 type ConvertState = {
   from: TCurrencyInfo;
   to: TCurrencyInfo;
   setCurrency: (currencyInfo: TCurrencyInfo, type: "from" | "to") => void;
+  switchCurrencies: () => void;
 };
 
-const useConvertStore = create<ConvertState>((set) => ({
-  from: { country: "US", name: "US Dollars", symbol: "USD" },
-  to: { country: "EU", name: "Euros", symbol: "EUR" },
-  setCurrency: (currencyInfo, type) => {
-    if (type === "from") {
-      set({ from: currencyInfo });
-    } else {
-      set({ to: currencyInfo });
-    }
-  },
-}));
+const useConvertStore = create<ConvertState>()(
+  devtools((set, get) => ({
+    from: { country: "US", name: "US Dollars", symbol: "USD" },
+    to: { country: "EU", name: "Euros", symbol: "EUR" },
+    setCurrency: (currencyInfo, type) => {
+      if (type === "from") {
+        set({ from: currencyInfo });
+      } else {
+        set({ to: currencyInfo });
+      }
+    },
+    switchCurrencies: () => {
+      set({ from: get().to, to: get().from });
+    },
+  }))
+);
 
 export default useConvertStore;
