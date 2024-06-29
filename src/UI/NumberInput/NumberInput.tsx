77171f3a19fb63eceba4dd8863faed4079344fe1
Mikhail <Mikhail.Yakushinale@Gmail.com>
2024-06-29 17:07:47 +0700

    refactor: refactored UI input component

--- src/UI/NumberInput/NumberInput.tsx
+++ src/UI/NumberInput/NumberInput.tsx
@@ -1,16 +1,15 @@
-import { ChangeEvent, FC, memo } from "react";
+import { FC, InputHTMLAttributes, memo } from "react";
 import clsx from "clsx";
 import styles from "./NumberInput.module.scss";
 
 type NumberInputProps = {
   label: string;
   placehodler?: string;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   extraStyles?: string;
-};
+} & InputHTMLAttributes<HTMLInputElement>;
 
 const NumberInput: FC<NumberInputProps> = memo((props) => {
-  const { label, placehodler, onChange, extraStyles } = props;
+  const { label, placehodler, extraStyles, onChange, value } = props;
   const style: string = clsx(styles.numberInput, extraStyles || "");
   const placehodlerCond: string | undefined = placehodler ? placehodler : undefined;
 
@@ -21,7 +20,8 @@ const NumberInput: FC<NumberInputProps> = memo((props) => {
         className={styles.numberInput__input}
         type="number"
         placeholder={placehodlerCond}
-        onChange={(e) => onChange(e)}
+        value={value}
+        onChange={onChange}
       />
     </form>
   );
