d3783ff946fe64dd45ee0c034d83707c98579b25
Mikhail <Mikhail.Yakushinale@Gmail.com>
2024-07-10 21:27:58 +0700

    fix: the count of numbers after the decimal point in currency conversion function was set to 2

--- src/modules/ConvertDisplay/utils/convert.ts
+++ src/modules/ConvertDisplay/utils/convert.ts
@@ -1,5 +1,5 @@
-const convert = (rate: number, amount: number): number => {
-  return amount * rate;
+const convert = (rate: number, amount: number): string => {
+  return (amount * rate).toFixed(2);
 };
 
 export default convert;
